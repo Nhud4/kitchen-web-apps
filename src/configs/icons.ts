d97fbc01b2d3fb6d4be6fc7321758fba503d4e91
Muhammad Nurul Huda <nu12ulhud4@gmail.com>
2026-01-20 16:05:39 +0700

    fix: icon

--- src/configs/icons.ts
+++ src/configs/icons.ts
@@ -1,25 +1,25 @@
 import Back from '@assets/icons/Back.svg?react'
+import Close from '@assets/icons/close.svg?react'
 import Eye from '@assets/icons/eye.svg?react'
 import EyeSlash from '@assets/icons/eye-slash.svg?react'
+import Home from '@assets/icons/home.svg?react'
 import Lock from '@assets/icons/lock.svg?react'
 import Logo from '@assets/icons/Logo.svg?react'
 import Printer from '@assets/icons/printer.svg?react'
-import Home from '@assets/icons/home.svg?react'
 import Reload from '@assets/icons/reload.svg?react'
 import Search from '@assets/icons/search.svg?react'
-import Close from '@assets/icons/close.svg?react'
 
 const ICONS = {
   Back,
+  Close,
   Eye,
   EyeSlash,
+  Home,
   Lock,
   Logo,
   Printer,
-  Home,
   Reload,
   Search,
-  Close
 }
 
 export default ICONS
