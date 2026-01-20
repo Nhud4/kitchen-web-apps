05684e2070797256370f0414c1c7774e20523a64
Muhammad Nurul Huda <nu12ulhud4@gmail.com>
2026-01-20 12:51:37 +0700

    fix: login form

--- src/components/forms/Login/utils.ts
+++ src/components/forms/Login/utils.ts
@@ -2,14 +2,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
 
 const loginSchema = z.object({
-  password: z
-    .string()
-    .nonempty('Password harus diisi.')
-    .min(8, { message: 'Password harus lebih dari 8 karakter.' }),
-  username: z
-    .string()
-    .nonempty('Username harus diisi.')
-    .min(4, { message: 'Username harus lebih dari 4 karakter.' }),
+  password: z.string().nonempty('Password harus diisi.'),
+  username: z.string().nonempty('Username harus diisi.'),
 })
 
 export const resolver = zodResolver(loginSchema)
